b42d028687365d24a84ebb8cdef17f9e6b717cbe
Naman Agarwal <namana019@outlook.com>
2020-04-08 01:30:17 +0530

    Deleted the previous template due to dimension issues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import PDFViewer from './Containers/PDFViewer/pdfViewer';
 import './App.css';
 
 class App extends React.Component {
  
-  exportPDF = () => {
-    this.resume.save();
-}
-
   render(){
     return (
       <div className="App">
-        <PDFViewer />
       </div>
     );
   }
