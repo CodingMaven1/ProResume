cb3b8325ead8fe9355bb1e2c42ae646b20221297
Naman Agarwal <namana019@outlook.com>
2020-04-15 20:35:29 +0530

    Working on Career Input

---
+++ src/Containers/LeftNav/LeftNavComponents/Career/Career.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Career.scss';
+
+class Career extends React.Component{
+    render(){
+        return(
+            <div className="Career">
+
+            </div>
+        )
+    }
+}
+
+export default Career;
